2fd376af8fe1a714483d61d2c017c6bb7c685f44
Carlos Gonzalez <contactos503@gmail.com>
2020-06-13 15:31:23 -0600

    a la rama dev

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
+export const routes: Routes = [
+   
+    {path:'heroes', component:HeroesComponent},
+    {path:'heroe/:id', component:HeroeComponent},
+    {path:'**',pathMatch:'full', redirectTo: 'heroes'}
+
+]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
